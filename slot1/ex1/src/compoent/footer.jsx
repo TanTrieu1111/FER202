97ee83099c52654bec19a0346afd39148fed4713
Trieu <tantrieu1999@gmail.com>
2026-01-11 12:54:01 +0700

    lan6

---
+++ slot1/ex1/src/compoent/footer.jsx
@@ -0,0 +1,8 @@
+function Footer() {
+    return{
+        <>
+        <img src="logo.https://i.pinimg.com/736x/39/ba/f6/39baf6fd7cd6570aa0326bddf1c4b6bb.jpg" alt="Logo" width="100">
+            <p>© 2026 - Website của tôi</p>
+        </>
+    }
+}
---
+++ slot2/ex2/src/App.js
@@ -0,0 +1,10 @@
+import logo from './logo.svg';
+import './App.css';
+import About from './compoents/About';
+function App() {
+  return (
+    <About />
+  );
+}
+
+export default App;
