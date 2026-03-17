2c47b201c0cba6ae5aa8fac82706e9422f1dc2c3
Trieu <tantrieu1999@gmail.com>
2026-03-17 18:26:40 +0700

    lan2

--- fer202/reudxtoolkit/src/routes/ProtectedRoute.jsx
+++ fer202/reudxtoolkit/src/routes/ProtectedRoute.jsx
@@ -1,15 +1,15 @@
-import { Navigate } from "react-router-dom";
-import { useSelector } from "react-redux";
+import { useSelector } from 'react-redux';
+import { Navigate } from 'react-router-dom';
 
-const ProtectedRoute = ({ children }) => {
+function ProtectedRoute({ children }) {
+    const isAuthenticated = useSelector(
+        (state) => state.auth.isAuthenticated
+    );
 
-    const user = useSelector((state) => state.auth.user);
-
-    if (!user) {
-        return <Navigate to="/" />;
+    if (!isAuthenticated) {
+        return <Navigate to="/login" />;
     }
-
     return children;
-};
+}
 
 export default ProtectedRoute;
